32304190246be215febf8365f144bc05940225fc
Scott Hill <scott.c.hill.jr@gmail.com>
2022-01-03 15:20:14 -0600

    commit tree 1921364452108d8a3eff367a8928d5c4eee2d9d5

---
+++ index.js
@@ -0,0 +1,11 @@
+let number1 = 2000
+let number2 = 100
+const divide = function() {
+    return number1 / number2;
+}
+divide();
+
+const square = number3 => (number3 * number3)
+
+const add = (number4, number5) => (number4 + number5)
+
